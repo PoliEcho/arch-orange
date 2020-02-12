35282b8ad6272d7bc5e6905bfaa863c48b41106e
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-02-12 13:23:29 +0100

    GTK3 theme: Add Gedit custom widget styles

--- src/themes/Kali/sass/gtk-sass/applications/nautilus/sync.sh
+++ src/themes/Kali/sass/gtk-sass/applications/nautilus/sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget https://gitlab.gnome.org/GNOME/nautilus/raw/master/src/resources/css/Adwaita.css --timestamping
+wget https://gitlab.gnome.org/GNOME/nautilus/raw/master/src/resources/css/Adwaita.css -O Adwaita.css
 
