2d1d8e0b1f40d7b20097451e28e6aa3231e6a90e
Daniel Ruiz de Alegría <daniruiz@kali.org>
2021-10-04 15:18:15 +0200

    KDE: Add new kwin theme based on breeze

---
+++ KDE-themes/kdecoration/breeze.h
@@ -0,0 +1,61 @@
+#ifndef breeze_h
+#define breeze_h
+
+/*
+ * SPDX-FileCopyrightText: 2014 Hugo Pereira Da Costa <hugo.pereira@free.fr>
+ *
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
+
+#include "breezesettings.h"
+
+#include <QSharedPointer>
+#include <QList>
+
+namespace Breeze
+{
+    //* convenience typedefs
+    using InternalSettingsPtr = QSharedPointer<InternalSettings>;
+    using InternalSettingsList = QList<InternalSettingsPtr>;
+    using InternalSettingsListIterator = QListIterator<InternalSettingsPtr>;
+
+    //* metrics
+    enum Metrics
+    {
+
+        //* corner radius (pixels)
+        Frame_FrameRadius = 4,
+
+        //* titlebar metrics, in units of small spacing
+        TitleBar_TopMargin = 3,
+        TitleBar_BottomMargin = 3,
+        TitleBar_SideMargin = 1,
+        TitleBar_ButtonSpacing = 2,
+
+        Frame_BorderSize = 2,
+
+        // shadow dimensions (pixels)
+        Shadow_Overlap = 3,
+
+    };
+
+    //* standard pen widths
+    namespace PenWidth
+    {
+        /* Using 1 instead of slightly more than 1 causes symbols drawn with
+         * pen strokes to look skewed. The exact amount added does not matter
+         * as long as it isn't too visible.
+         */
+        // The standard pen stroke width for symbols.
+        static constexpr qreal Symbol = 1.01;
+    }
+
+    //* exception
+    enum ExceptionMask
+    {
+        None = 0,
+        BorderSize = 1<<4
+    };
+}
+
+#endif
